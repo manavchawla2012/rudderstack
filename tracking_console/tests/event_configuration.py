8b46d5ae372208f420554839da77b84bf878a28f
manav.chawla <manav.chawla@smartstaff.co.in>
2023-03-13 00:43:38 +0530

    added test case for failure event

--- tracking_console/tests/event_configuration.py
+++ tracking_console/tests/event_configuration.py
@@ -1,7 +1,9 @@
 from django.test import TestCase
+from rest_framework.serializers import ValidationError
 from faker import Faker
 
 from users.models import UserModel
+from tracking_console.serializers import EventConfigurationSerializer
 
 
 class EventConfiguration(TestCase):
@@ -13,8 +15,8 @@ class EventConfiguration(TestCase):
         self.user.set_password(self.fake.password())
         self.user.save()
 
+    # It should allow to create a event config event
     def test_create_event_config_correct_data(self):
-        from tracking_console.serializers import EventConfigurationSerializer
         obj = EventConfigurationSerializer(data={
             "name": self.fake.name(),
             "description": self.fake.name(),
@@ -46,3 +48,37 @@ class EventConfiguration(TestCase):
         obj.is_valid(raise_exception=True)
         obj.save()
         self.assertIsNotNone(obj.instance.id)
+
+    # It should throw error when required field is not part of property
+    def test_event_config_required_field_not_in_property(self):
+        obj = EventConfigurationSerializer(data={
+            "name": self.fake.name(),
+            "description": self.fake.name(),
+            "created_by": self.user.id,
+            "rules": {
+                "schema": "http://json-schema.org/draft-07/schema#",
+                "object_type": "object",
+                "properties": {
+                    "object_type": "object",
+                    "properties": {
+                        "product": {
+                            "data_type": ["string"]
+                        },
+                        "price": {
+                            "data_type": ["number"]
+                        },
+                        "currency": {
+                            "data_type": ["string"]
+                        }
+                    },
+                    "required": [
+                        "product1",
+                        "price",
+                        "currency"
+                    ]
+                }
+            }
+        })
+        with self.assertRaises(ValidationError):
+            obj.is_valid(raise_exception=True)
+            obj.save()
